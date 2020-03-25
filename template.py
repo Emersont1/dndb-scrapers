2e0f77ab374ee1203ae17a95a71f0aaca50c8a7a
Peter Taylor <info@emersont1.co.uk>
2020-03-25 17:49:47 +0000

    added template file

---
+++ template.py
@@ -0,0 +1,13 @@
+import common
+import sys
+
+if len(sys.argv) == 1:
+    db_path = "pathfinder.sqlite3"
+else:
+    db_path = sys.argv[1]
+
+def scrape_type(path):
+    conn = common.open_db(db_path)
+
+if __name__ == "__main__":
+    scrape_type(db_path)
