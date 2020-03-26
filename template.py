8fb9893789ff164bb5e98417289c95628d6ec874
Emersont1 <info@emersont1.co.uk>
2020-03-26 14:55:00 +0000

    Update template.py

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ else:
     db_path = sys.argv[1]
 
 def scrape_type(path):
-    conn = common.open_db(db_path)
+    conn = common.open_db(path)
 
 if __name__ == "__main__":
     scrape_type(db_path)
