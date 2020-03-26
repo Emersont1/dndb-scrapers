d4267871e10a59d812ba757fc3f6a9850de8326d
Peter Taylor <info@emersont1.co.uk>
2020-03-26 13:50:47 +0000

    beautified

--- armour.py
+++ armour.py
@@ -7,6 +7,7 @@ if len(sys.argv) == 1:
 else:
     db_path = sys.argv[1]
 
+
 def scrape_armour(path):
     conn = common.open_db(db_path)
     db = conn.cursor()
@@ -42,40 +43,46 @@ def scrape_armour(path):
             main_c = 0
             sources = []
             for child in td.find("span").children:
-                    if child.name == "br" and child.next_sibling.name != "b":
-                        main_c = child.next_sibling
-                        break
-                    if child.name != "b":
-                        continue
-                    if child.string == "Source":
-                        x = child.next_sibling
-                        while x.name != "h3":
-                            if x.name == "a":
-                                sources.append(common.reference(db,x))
-                            x = x.next_sibling
-                        continue
-                    if child.string in [
-                        "Cost",
-                        "Weight",
-                        "Armor Bonus",
-                        "Max Dex Bonus",
-                        "Armor Check Penalty",
-                        "Arcane Spell Failure Chance",
-                    ]:
-                        array_item = child.string.lower().replace(" ", "_").replace("armor", "armour") # okay that last bit is pedantry, BUT
-                        data[array_item] = common.integer_property(child.next_sibling.string)
-                        continue
-                    if child.string == "Speed":
-                        speeds = child.next_sibling.string.split("/")
-                        data["speed_30"] = common.integer_property(speeds[0])
-                        data["speed_20"] = common.integer_property(speeds[1])
-                        continue
-                    print(child.string + ": " + child.next_sibling.string)
+                if child.name == "br" and child.next_sibling.name != "b":
+                    main_c = child.next_sibling
+                    break
+                if child.name != "b":
+                    continue
+                if child.string == "Source":
+                    x = child.next_sibling
+                    while x.name != "h3":
+                        if x.name == "a":
+                            sources.append(common.reference(db, x))
+                        x = x.next_sibling
+                    continue
+                if child.string in [
+                    "Cost",
+                    "Weight",
+                    "Armor Bonus",
+                    "Max Dex Bonus",
+                    "Armor Check Penalty",
+                    "Arcane Spell Failure Chance",
+                ]:
+                    array_item = (
+                        child.string.lower()
+                        .replace(" ", "_")
+                        .replace("armor", "armour")
+                    )  # okay that last bit is pedantry, BUT
+                    data[array_item] = common.integer_property(
+                        child.next_sibling.string
+                    )
+                    continue
+                if child.string == "Speed":
+                    speeds = child.next_sibling.string.split("/")
+                    data["speed_30"] = common.integer_property(speeds[0])
+                    data["speed_20"] = common.integer_property(speeds[1])
+                    continue
+                print(child.string + ": " + child.next_sibling.string)
             my_str = ""
             u = None
             for header in td.find_all("h3"):
                 if header.string == "Description":
-                    u=header.next_sibling
+                    u = header.next_sibling
             while u != None:
                 my_str += str(u)
                 u = u.next_sibling
@@ -83,14 +90,20 @@ def scrape_armour(path):
 
             data["description"] = common.cleanup_html(my_str)
 
-            db.execute("INSERT INTO armour (name, cost, weight, armour_bonus, max_dex, check_pen, arcane_failure_chance, speed_30, speed_20, description) VALUES (:name, :cost, :weight, :armour_bonus, :max_dex_bonus, :armour_check_penalty, :arcane_spell_failure_chance, :speed_30, :speed_20, :description);", data)
+            db.execute(
+                "INSERT INTO armour (name, cost, weight, armour_bonus, max_dex, check_pen, arcane_failure_chance, speed_30, speed_20, description) VALUES (:name, :cost, :weight, :armour_bonus, :max_dex_bonus, :armour_check_penalty, :arcane_spell_failure_chance, :speed_30, :speed_20, :description);",
+                data,
+            )
             db.execute("SELECT last_insert_rowid();")
             rows = db.fetchall()
             i = rows[0][0]
             for x in sources:
                 x.append(i)
                 print(x)
-                db.execute("INSERT INTO reference (table_name, source, page, destination) VALUES ('armour', ?, ?, ?);", x)
+                db.execute(
+                    "INSERT INTO reference (table_name, source, page, destination) VALUES ('armour', ?, ?, ?);",
+                    x,
+                )
     conn.commit()
     conn.close()
 
--- common.py
+++ common.py
@@ -5,24 +5,31 @@ import os
 import re  # eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 import sqlite3
 
+
 def url_esc(url):
-    return url.replace("/","_")
+    return url.replace("/", "_")
+
 
 def get_file(url):
     if not os.path.exists("__scraping_cache__"):
         os.mkdir("__scraping_cache__")
-    if not os.path.exists("__scraping_cache__/"+url_esc(url)):
+    if not os.path.exists("__scraping_cache__/" + url_esc(url)):
         request = requests.get(url)
-        file_content = request.content.decode(request.encoding).encode('ascii', 'xmlcharrefreplace').decode('ascii')
-        f = open("__scraping_cache__/"+url_esc(url), "w+")
+        file_content = (
+            request.content.decode(request.encoding)
+            .encode("ascii", "xmlcharrefreplace")
+            .decode("ascii")
+        )
+        f = open("__scraping_cache__/" + url_esc(url), "w+")
         f.write(file_content)
         f.close()
         return file_content
     else:
-        f = open("__scraping_cache__/"+url_esc(url), "r")
+        f = open("__scraping_cache__/" + url_esc(url), "r")
         file_content = f.read()
         f.close()
         return file_content
+
 
 def open_db(path):
     if os.path.isfile(path):
@@ -31,31 +38,36 @@ def open_db(path):
         conn = sqlite3.connect(path)
         # We Have to execute some SQL on this
         db = conn.cursor()
-        with open('D&Db.sql', 'r') as file:
+        with open("D&Db.sql", "r") as file:
             data = file.read()
             db.executescript(data)
         return conn
+
 
 def integer_property(x):
     fixed_string = re.sub("[,;%]", "", x).replace("- ", "-")
     strings = fixed_string.split(" ")
     for s in strings:
         if s != "":
-            if s=="—":
+            if s == "—":
                 return None
             else:
                 return int(s)
 
+
 def reference(db, link):
     s = link.string
-    q=s.split(" pg. ")
+    q = s.split(" pg. ")
     db.execute("SELECT id from source WHERE name = ?;", (q[0],))
     rows = db.fetchall()
     if len(rows) == 0:
-        db.execute("INSERT INTO source (name, url) VALUES (?,?);",[q[0], link.get("href")])
+        db.execute(
+            "INSERT INTO source (name, url) VALUES (?,?);", [q[0], link.get("href")]
+        )
         db.execute("SELECT last_insert_rowid();")
         rows = db.fetchall()
     return [rows[0][0], int(q[1])]
+
 
 def cleanup_html(string):
     # stub, to be fixed later
